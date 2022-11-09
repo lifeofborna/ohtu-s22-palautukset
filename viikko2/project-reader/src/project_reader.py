ada45879d42c986fd845517cce0bb6231687cfe4
Borna <borna.jamali@gmail.com>
2022-11-09 11:06:47 +0200

    Tehtävät ennen 8

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,15 @@
+import toml
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = toml.loads(request.urlopen(self._url).read().decode("utf-8"))
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(content['tool']['poetry']["name"], content['tool']['poetry']["description"], content['tool']['poetry']["dependencies"], content['tool']['poetry']["dev-dependencies"])
