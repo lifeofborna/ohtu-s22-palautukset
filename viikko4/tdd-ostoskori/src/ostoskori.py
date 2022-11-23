fc0a97227f6b306bf4c5d927904111795ceb3896
Borna <borna.jamali@gmail.com>
2022-11-23 10:07:14 +0200

    3. Loin testin jossa lisätään yksi tuote ja tarkistetaan että korin hinta on tuotteen hinta. Tämän jälkeen koodasin ostoskorin hinta metodin että palauttaa ensimmäisen tuotteen hinnan jos ei tyhjä.

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -13,7 +13,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        if len(self.ostoskori) >= 1:
+            return self.ostoskori[0].hinta()
+        else:
+            return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,14 +5,18 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-
+        self.auto = Tuote("auto",1)
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(),0)
 
 
     def test_yksi_tuote_yksi_tavara(self):
-        auto = Tuote("auto",1)
-        self.kori.lisaa_tuote(auto)
+        self.kori.lisaa_tuote(self.auto)
 
         self.assertEqual(self.kori.tavaroita_korissa(),1)
+    
+    def test_ostoskorin_hinta_sama_kuin_tuotteen(self):
+        self.kori.lisaa_tuote(self.auto)
+
+        self.assertEqual(self.auto.hinta(), self.kori.hinta())
