35fbace72e706eeaa21d574d10c2c8fe03c4ca65
Borna <borna.jamali@gmail.com>
2022-11-23 10:24:20 +0200

    8. Loin testin yhden tuotteen lisäämisen jälkeen ostoskorissa on yksi ostos. Tämän jälkeen koodasin ostokset metodin palauttamalla self.ostoskori lista.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -44,3 +44,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.auto)
 
         self.assertEqual(self.kori.hinta(),self.auto.hinta()*2)
+    
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        # testaa että metodin palauttaman listan pituus 1
+        self.assertEqual(len(ostokset), 1)
