521c7aa1905b14d8de03a0a12ea4ad912fb96d08
Borna <borna.jamali@gmail.com>
2022-11-23 10:53:43 +0200

    14. Loin testin jossa koriin lisätty tuote joka poistetaan niin tällöin kori on tyhjä. Ei tarvitsenut koodia ostoskoriin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -92,3 +92,15 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(ostokset[0].lukumaara(),1)
+    
+    def test_jos_korissa_1_tuote_ja_poistetaan_kori_tyhja(self):
+        maito = Tuote("Maito", 3)
+
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 0)
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(len(self.kori.ostoskori), 0)
